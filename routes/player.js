9ff0ee9a317d03c611d960b3585a7494896bf9e7
JoshMBN <joshua.brennan.niu@gmail.com>
2024-08-26 18:54:46 +1200

    Fix acount route post => get

--- routes/player.js
+++ routes/player.js
@@ -4,6 +4,6 @@ const router = Router();
 import { getPlayerInfo } from "../controllers/v1/player.js";
 import authRoute from "../middleware/authRoute.js"
 
-router.route("/account").post(authRoute, getPlayerInfo);
+router.route("/account").get(authRoute, getPlayerInfo);
 
 export default router;

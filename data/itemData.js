9133ff3e1addaf82ecc6b3ee04109c9a4bbfff58
JoshMBN <joshua.brennan.niu@gmail.com>
2024-08-29 16:04:50 +1200

    Added initial Item Data file.

---
+++ data/itemData.js
@@ -0,0 +1,5 @@
+export const potionsData = [
+    {name: "Minor Health Potion", type: "Healing", healing: 40, buyPrice: 200, sellPrice: 80},
+    {name: "Major Health Potion", type: "Healing", healing: 100, buyPrice: 550, sellPrice: 240},
+    {name: "Potion of Luck", type: "Buff", luck: 5, buyPrice: 2000, sellPrice: 600}
+]

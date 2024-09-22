bffdbdc6308b946814608f2c2eb1217f86932617
fuzzyers <104363831+fuzzyers@users.noreply.github.com>
2024-09-22 22:40:50 +1200

    Updated Schema and made a create

--- routes/adventure.js
+++ routes/adventure.js
@@ -3,8 +3,10 @@ const router = Router()
 
 import { goToZone, zoneGeneration, deleteZone, collect } from "../controllers/v1/adventure.js"
 import authRoute from "../middleware/authRoute.js"
+import { createQuest } from "../controllers/v1/quest.js";
 
 router.route("/zone").post(authRoute, zoneGeneration)
 router.route("/zone/:zoneid").delete(authRoute, deleteZone).put(authRoute, collect)
 router.route("/goToZone/:zoneid").put(authRoute, goToZone)
+router.route("/quest").post(authRoute, createQuest)
 export default router
